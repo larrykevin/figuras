3ad7958fefb149eff8f20d7b16a8ce9da1507100
Larry Caycho <larry.kevin.c.c@gmail.com>
2021-08-21 13:39:47 -0500

    Regresando a funciones dentro de figuras.js

--- figuras.js
+++ figuras.js
@@ -112,7 +112,7 @@ function redondearDecimales(numero, decimales) {
     numeroRegexp = new RegExp('\\d\\.(\\d){' + decimales + ',}');
     // Expresion regular para números con un cierto número de decimales o mas
     if (numeroRegexp.test(numero)) {
-        // Ya que el numero tiene el número de decimales requeridos o mas, se realiza el redondeo
+        // Ya que el número tiene el número de decimales requeridos o mas, se realiza el redondeo
         return Number(numero.toFixed(decimales));
     } else {
         return Number(numero.toFixed(decimales)) === 0 ? 0 : numero;
